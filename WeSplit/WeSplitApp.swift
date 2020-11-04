d8b0421bb21fa6d540a5a3ebbe933759cfb0af40
ameliapeng <apeng@lcs.on.ca>
2020-11-04 10:51:23 -0500

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by amelia peng on 2020-11-04.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
